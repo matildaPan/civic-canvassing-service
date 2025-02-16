d1d37dd771737672a5f93929cdd5f01c2917ed04
Matilda Pan <matilda.yipan@gmail.com>
2025-02-15 23:18:36 -0500

    add grapgql schema and resolver for apollo server

---
+++ src/resolvers.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from "@prisma/client";
+import { PubSub } from "graphql-subscriptions";
+import GraphQLJSON from "graphql-type-json";
+
+const prisma = new PrismaClient();
+const pubsub = new PubSub() as any;
+
+export const resolvers = {
+  JSON: GraphQLJSON,
+
+  Query: {
+    getHouseholds: async () => await prisma.household.findMany(),
+    getResponses: async (_: any, { householdId }: { householdId: string }) =>
+      await prisma.response.findUnique({ where: { householdId } }),
+  },
+
+  Mutation: {
+    addHousehold: async (_: any, { address }: { address: string }) => {
+      const household = await prisma.household.create({ data: { address } });
+      pubsub.publish("HOUSEHOLD_UPDATED", { householdUpdated: household });
+      return household;
+    },
+
+    removeHousehold: async (_: any, { id }: { id: string }) => {
+      await prisma.household.delete({ where: { id } });
+      pubsub.publish("HOUSEHOLD_UPDATED", { householdUpdated: null });
+      return true;
+    },
+
+    submitResponse: async (_: any, { householdId, answers }: any) => {
+      const response = await prisma.response.create({
+        data: { householdId, answers },
+      });
+
+      await prisma.household.update({
+        where: { id: householdId },
+        data: { completed: true },
+      });
+
+      pubsub.publish("HOUSEHOLD_UPDATED", {
+        householdUpdated: await prisma.household.findUnique({
+          where: { id: householdId },
+        }),
+      });
+
+      return response;
+    },
+  },
+
+  Subscription: {
+    householdUpdated: {
+      subscribe: () => pubsub.asyncIterator(["HOUSEHOLD_UPDATED"]),
+    },
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import cors from "cors";
+import { WebSocketServer } from "ws";
+import { PubSub } from "graphql-subscriptions";
+import { PrismaClient } from "@prisma/client";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { readFileSync } from "fs";
+import { gql } from "graphql-tag";
+import { resolvers } from "./resolvers";
+import { useServer } from "graphql-ws/lib/use/ws";
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const typeDefs = gql(readFileSync("src/schema.graphql", "utf8"));
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const server = new ApolloServer({ schema });
+
+const pubsub = new PubSub();
+
+async function startServer() {
+  await server.start();
+  app.use("/graphql", expressMiddleware(server));
+
+  const httpServer = app.listen(4000, () => {
+    console.log("🚀 Server running on http://localhost:4000/graphql");
+  });
+
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/graphql",
+  });
+
+  useServer({ schema, execute: undefined, subscribe: undefined }, wsServer);
+}
+
+startServer();
